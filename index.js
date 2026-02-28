ef0204b4315921ca1bf25353e1c0d42726aab562
fauzankaryadi61-commits <fauzankaryadi61@gmail.com>
2026-02-28 22:10:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -207,8 +207,7 @@ client.once("ready", async () => {
        choices: [
          { name: "5", value: 5 },
          { name: "10", value: 10 },
-         { name: "15", value: 15 },
-         { name: "20", value: 20 }
+         { name: "15", value: 15 }
         ]
       }
     ]
